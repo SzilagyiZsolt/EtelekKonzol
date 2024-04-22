e66270843b672385e70fc41945b5c695a350687a
SzilagyiZsolt <zsoltika20030@gmail.com>
2024-04-22 09:58:19 +0200

    Add project files.

---
+++ Etelek/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etelek
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<Leves> lev = new List<Leves>();
+            lev=await etelek();
+            Console.WriteLine($"{lev.Count()} leves található");
+            int maxKaloria = int.MinValue;
+            foreach (Leves levesek in lev)
+            {
+                if (levesek.Kaloria > maxKaloria)
+                {
+                    maxKaloria = (int)levesek.Kaloria;
+                    await Console.Out.WriteLineAsync($"{levesek.Megnevezes} rendelkezik a legnagyobb kalóriával");
+                }
+            }
+            Console.ReadKey();
+        }
+        private static async Task<List<Leves>> etelek()
+        {
+            List<Leves> lev = new List<Leves>();
+            string url = $"https://retoolapi.dev/Kc6xuH/data";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonString = await response.Content.ReadAsStringAsync();
+                lev = Leves.FromJson(jsonString).ToList();
+            }
+            return lev;
+        }
+    }
+}
